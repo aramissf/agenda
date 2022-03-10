6d332939bcf118324979667f39f1312c2040da90
Aramis Souza Farias <aramis.farias@unicb.coop.br>
2022-03-10 16:48:28 -0300

    1°

---
+++ contatos/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.core.paginator import Paginator
+from .models import Contato
+
+# Create your views here.
+
+
+def index(request):
+    contatos = Contato.objects.order_by('-id').filter(mostrar = True)
+    paginator = Paginator(contatos, 5)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/index.html', {
+        'contatos':contatos
+
+    })
+
+def ver_contato(request, contato_id):
+    contato=get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
+    return render(request, 'contatos/ver_contato.html',{
+        'contato':contato
+     })
